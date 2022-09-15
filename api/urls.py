e170d39a2066fd904667513df156fe425d951fab
HaroldObasi <haroldobasi2k16@gmail.com>
2022-09-15 10:57:01 +0100

    removed 2 views

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,7 @@ from . import views
 
 pathname = "api/"
 urlpatterns = [
-    path('', views.ExampleView.as_view()),  
     path('messages/', views.MessagesListView.as_view()),
-    path('projects/', views.ProjectListCreateView.as_view()),
+    path('projects/', views.ProjectsListView.as_view()),
     path('createProjects/', views.CreateProject.as_view()),
-    path('testS3/', views.TestingUploadView.as_view())
 ]
